f3210dadd4966999fcb479665706bdfcf8b3f628
Craig Ulliott <craigulliott@gmail.com>
2011-12-27 19:15:38 -0600

    more specific active helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -49,7 +49,11 @@ module ApplicationHelper
   # return 'active' if the current controller name matches the provided controller_name
   def active_for_controller controller_name
     params[:controller].to_s == controller_name.to_s ? 'active' : ''
-  
+  end
+
+  # return 'active' if the current controller and action name matches the provided controller_name and action_name
+  def active_for_controller_and_action controller_name, action_name
+    params[:controller].to_s == controller_name.to_s && params[:action].to_s == action_name.to_s ? 'active' : ''
   end
 
   #  string representation of how long ago an object was created, or 'never' if the object doesnt exist
