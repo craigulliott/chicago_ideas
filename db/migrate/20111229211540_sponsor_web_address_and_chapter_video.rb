cb7a9f25e8f8f0cc3977cb87baa41ae1cada0c60
Craig Ulliott <craigulliott@gmail.com>
2011-12-29 15:32:37 -0600

    typo

--- db/migrate/20111229211540_sponsor_web_address_and_chapter_video.rb
+++ db/migrate/20111229211540_sponsor_web_address_and_chapter_video.rb
@@ -1,11 +1,11 @@
 class SponsorWebAddressAndChapterVideo < ActiveRecord::Migration
   def up
     add_column :sponsors, :url, :string
-    add_column :chapter, :vimeo_id, :string
+    add_column :chapters, :vimeo_id, :string
   end
 
   def down
     remove_column :sponsors, :url
-    remove_column :chapter, :vimeo_id
+    remove_column :chapters, :vimeo_id
   end
 end
