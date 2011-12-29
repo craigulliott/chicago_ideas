8166aad2e0cb07175b1b02c908f85d505e10308f
Craig Ulliott <craigulliott@gmail.com>
2011-12-29 12:52:03 -0600

    assets for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ CraigsAdmin::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( application.js application.css admin.css admin.js api.css api.js )
+  config.assets.precompile += %w( application.js application.css admin.css admin.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
