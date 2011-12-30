34c1d541bc7b68b73081c25a64390f8c95952659
Craig Ulliott <craigulliott@gmail.com>
2011-12-30 16:11:37 -0600

    enabling basic page caching and memcache

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,8 @@
 // =require common
 
 // =require application/contact
+
+// all pages on the front end of the website have the dynamic account links lazy loaded, this allows for the rest of the page to be cached by varnish, squid etc. and results in extremely fasy user experience
+$(document).ready(function(){
+  $('#account_links').load('/account_links');
+});
