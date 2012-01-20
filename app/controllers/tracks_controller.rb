cd9a9dad0a027a51ed15dc15bed13d9247d8e618
Craig Ulliott <craigulliott@gmail.com>
2012-01-20 13:43:31 -0600

    talks under tracks

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -6,6 +6,14 @@ class TracksController < ApplicationController
   
   
   def show
+    @track = Track.find_by_name(params[:id])
+    @tracks = Track.search_sort_paginate(params)
+    
+    @talks = @track.talks.search_sort_paginate(params)
+    @megatalks = TalkBrand.find_by_name("Mega Talk").talks
+
+    @speakers = User.speaker.limit(6)
+    render "talks/index"
   
   end
   
