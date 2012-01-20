270364fe5896720ffcaf581d1ff79d6322081b08
Craig Ulliott <craigulliott@gmail.com>
2012-01-20 10:58:43 -0600

    trying to fix production assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ CraigsAdmin::Application.configure do
 
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
-  
+  config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
@@ -16,8 +16,6 @@ CraigsAdmin::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
-
-  config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
