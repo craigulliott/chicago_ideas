67ad86ee5f813cf3aabf4d11df2471a7c08280a2
Craig Ulliott <craigulliott@gmail.com>
2012-01-20 10:21:52 -0600

    trying to fix production assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ CraigsAdmin::Application.configure do
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
 
-  config.assets.initialize_on_precompile = true
+  config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
