4ab037d8d71d10d24108994cb3974b910f679dc5
John Koht <johnkoht@Johns-iMac.local>
2012-01-20 12:53:52 -0600

    updating controllers for page titles and caching

--- app/controllers/sponsors_controller.rb
+++ app/controllers/sponsors_controller.rb
@@ -1,7 +1,12 @@
 class SponsorsController < ApplicationController
 
+  # cache rendered versions of these pages
+  before_filter :cache_rendered_page, :only => [:index]
+
+
   def index
     @sponsorshipLevels = SponsorshipLevel.order("sort").all
+    @page_title = "Sponsors"
   end
   
   
--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,7 +1,11 @@
 class TracksController < ApplicationController
+
+  # cache rendered versions of these pages
+  before_filter :cache_rendered_page, :only => [:index, :show]
   
   def index
     @tracks = Track.search_sort_paginate(params)
+    @page_title = "Topics"
   end
   
   
