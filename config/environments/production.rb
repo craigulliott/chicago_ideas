507adc3e6486541f18c6a9a76caf0dea56469e3a
Craig Ulliott <craigulliott@gmail.com>
2012-01-20 11:17:13 -0600

    trying to get caching working on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,9 +37,7 @@ CraigsAdmin::Application.configure do
   # config.logger = SyslogLogger.new
 
   # Use a different cache store in production
-  config.cache_store = :mem_cache_store, Memcached::Rails.new
-  $cache = Memcache.new
-  config.middleware.use Rack::Cache, :metastore => $cache, :entitystore => 'file:tmp/cache/entity'
+  config.cache_store = :dalli_store
   
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
