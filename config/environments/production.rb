c13f069920dad462e09fceaf19edb9399c6a6ee7
Craig Ulliott <craigulliott@gmail.com>
2012-01-20 09:29:43 -0600

    trying to fix production assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ CraigsAdmin::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   #config.assets.precompile += %w( application.js application.css admin.js admin.css )
-  config.assets.precompile += %w( admin.js admin.css )
+  config.assets.precompile += %w( application.js application.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
