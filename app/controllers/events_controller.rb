922522a49a49a0413cf856c01c030822464888c1
John Koht <johnkoht@Johns-iMac.local>
2012-01-20 12:53:52 -0600

    updating controllers for page titles and caching

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,11 @@
 class EventsController < ApplicationController
 
+  # cache rendered versions of these pages
+  before_filter :cache_rendered_page, :only => [:index, :labs, :partner_programs, :affiliate_event]
+
+
   def index
+    @page_title = "Events"
     @talks = Talk.limit(8)
     @chapters = Chapter.limit(8)
     @speakers = User.speaker.limit(12).order(rand) # grab 12 speakers
@@ -12,11 +17,13 @@ class EventsController < ApplicationController
   def labs
    @labs = EventBrand.find_by_name("Lab").events
    @event_photos = EventPhoto.order('RAND()').limit(10)
+   @page_title = "CIW Labs"
   end
   
   # Labs landing and individual pages
   def lab
     @lab = Event.find(params[:id])
+    @page_title = "#{@lab.title}"
   end
   
   
@@ -25,8 +32,10 @@ class EventsController < ApplicationController
   def partner_programs
     if params[:id].nil? # if no paramter, then load the labs landing
      @partnerprograms = EventBrand.find_by_name("Partner Program").events
+     @page_title = "Partner Programs"
     else
       @partnerprogram = Event.find(params[:id])
+      @page_title = "#{@partnerprogram.title}"
       render "application/partner-programs/partnerprograms_individual"
     end
   end
@@ -34,6 +43,7 @@ class EventsController < ApplicationController
   
   # Affiliate events landing
   def affiliate_event
+    @page_title = "Affiliate Events"
     @events = EventBrand.find_by_name("Affiliate Event").events
     @event_photos = EventPhoto.order('RAND()').limit(10)
   end
--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -1,7 +1,12 @@
 class PartnersController < ApplicationController
 
+  # cache rendered versions of these pages
+  before_filter :cache_rendered_page, :only => [:index, :apply]
+
+
   def index
     @partners = Partner.all
+    @page_title = "CIW Partners"
   end
   
   
