63be40a4d14ef7c0cb81849802cae29a2d68ec72
John Koht <john@kohactive.com>
2012-02-22 18:56:08 -0600

    updating the affiliate event and community partners forms and mailers

--- app/mailers/community_partners_mailer.rb
+++ app/mailers/community_partners_mailer.rb
@@ -1,11 +1,13 @@
 class CommunityPartnersMailer < ActionMailer::Base
 
   default :from => "forms@chicagoideas.com"
-  default :to => "john@kohactive.com"
+  default :to => "brooke@chicagoideas.com"
+  default :bcc => "john@kohactive.com"
   default :subject => "Affiliate Events Form Submission"
   
   def send_form(form)
-    mail(form)
+    @form = form
+    mail()
   end
   
 end
