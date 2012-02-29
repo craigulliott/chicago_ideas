820523416575dcd48a46d40abe20551a63ea9727
Justin McNally <justin@justin-dev.local>
2012-02-29 16:14:01 -0600

    stage api fixes

--- app/controllers/api/search_controller.rb
+++ app/controllers/api/search_controller.rb
@@ -1,2 +1,15 @@
 class Api::SearchController < Api::ApiController
+
+    def index
+    @query = params[:q]
+
+    respond_to do |format|
+      format.json { 
+        @results = ThinkingSphinx.search :conditions => {:name => @query, :title => @query}        
+        @results.collect!{|x| x.serializable_hash.merge!({:classType => x.class.to_s })}
+        render :json => @results
+      }
+    end
+  end
+  
 end
