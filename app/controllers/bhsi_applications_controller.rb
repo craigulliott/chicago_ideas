d8071d18e021608cdc4f75f5c32f8590df9d892e
Justin McNally <justin@justin-dev.local>
2012-03-07 17:02:06 -0600

    fix bhsi pdf gen

--- app/controllers/bhsi_applications_controller.rb
+++ app/controllers/bhsi_applications_controller.rb
@@ -16,7 +16,7 @@ class BhsiApplicationsController < ApplicationController
   def create
     @meta_data = {:page_title => "Bluhm/Helfand Social Innovation Fellowship", :og_image => "http://www.chicagoideas.com/assets/application/affilliate_events_banner.jpg", :og_title => "Bluhm/Helfand Social Innovation Fellowship | Chicago Ideas Week", :og_type => "website", :og_desc => "Chicago Ideas Week (CIW) is about the sharing of ideas, inspiring action and igniting change to positively impact our world. People who come to CIW are artists, engineers, technologists, inventors, scientists, musicians, economists, explorers-and, well...just innately passionate."}
     @bhsi_application = current_user.build_bhsi_application(params[:bhsi_application])
-    
+    @form = params[:bhsi_application];
     htmlContent = render_to_string :template => "#{self.controller_name}/pdf.html.erb", :layout => false
     
     pdf = doc_raptor_send({:document_type => "pdf".to_sym, :document_content => htmlContent})
