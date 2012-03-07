20f25ac6016b2c1be7044713027f401dc5f9011a
Justin McNally <justin@justin-dev.local>
2012-03-07 16:24:08 -0600

    fix validation on historical volunteers

--- app/mailers/affiliate_events_mailer.rb
+++ app/mailers/affiliate_events_mailer.rb
@@ -1,7 +1,7 @@
 class AffiliateEventsMailer < ActionMailer::Base
   
   default :from => "forms@chicagoideas.com"
-  default :to => "justin@kohactive.com"
+  default :to => "brooke@chicagoideas.com"
   default :bcc => "john@kohactive.com"
   default :subject => "Affiliate Events Form Submission"
   
