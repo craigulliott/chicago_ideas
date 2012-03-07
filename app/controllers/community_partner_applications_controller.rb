521b493841d423a74b66da07ccb68b151f3ae1c3
Justin McNally <justin@justin-dev.local>
2012-03-07 17:25:24 -0600

    merge

--- app/controllers/community_partner_applications_controller.rb
+++ app/controllers/community_partner_applications_controller.rb
@@ -32,7 +32,6 @@ class CommunityPartnerApplicationsController < ApplicationController
     
     if @community_partner_application.save!
       CommunityPartnersMailer.send_form(params[:community_partner_application], friendlyName).deliver
-      redirect_to root_path, :notice => 'Thank you, your application has been recieved.'
       #redirect_to root_path, :notice => 'Thank you, your application has been recieved.'
       render 'application/confirmation', :locals => {:title => "Community Partner Application Confirmation", :body => "Thank you for applying. We will be in contact shortly.", :url => "#{new_community_partner_application_path}" }
     else
