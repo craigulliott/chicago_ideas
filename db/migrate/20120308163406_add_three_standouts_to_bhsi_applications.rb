b8c1eeaecc6ebb5a6752346a9cd61d7bda816256
Justin McNally <justin@justin-dev.local>
2012-03-08 10:59:24 -0600

    clean up

--- db/migrate/20120308163406_add_three_standouts_to_bhsi_applications.rb
+++
@@ -1,5 +0,0 @@
-class AddThreeStandoutsToBhsiApplications < ActiveRecord::Migration
-  def change
-    add_column :bhsi_applications, :three_standout_statistics, :text, :null => false, :default => ""
-  end
-end
