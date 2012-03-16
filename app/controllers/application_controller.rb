915b6c4515e1a9a80d832b9e5b320754334b467d
John Koht <john@kohactive.com>
2012-03-16 12:09:15 -0500

    updating the sponsors stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,6 @@ class ApplicationController < ActionController::Base
   def index
     @talks = Talk.order('RAND()').limit(8)
     @speakers = User.speaker.not_deleted.order('RAND()').limit(8)
-    @sponsors = Sponsor.all
     @featured = Chapter.homepage_featured.order('RAND()').limit(8)
     @meta_data = {:page_title => "Welcome", :og_image => "http://www.chicagoideas.com/assets/application/logo.png", :og_title => "Chicago Ideas Week", :og_type => "website", :og_desc => "Chicago Ideas Week (CIW) is about the sharing of ideas, inspiring action and igniting change to positively impact our world. People who come to CIW are artists, engineers, technologists, inventors, scientists, musicians, economists, explorers-and, well...just innately passionate."}
   end
@@ -37,7 +36,7 @@ class ApplicationController < ActionController::Base
     @current_year = Year.find(2012)
   end
   def get_sponsors
-    @sponsors = Sponsor.featured_sponsors
+    @sponsors = Sponsor.featured_sponsors.order('RAND()')
   end
   def get_talks    
     @e_talks = TalkBrand.find(TALK_BRAND_ID).talks.order('RAND()').limit(10)
