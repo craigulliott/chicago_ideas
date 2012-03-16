325901d43fc3d83dd902b2ddc265c9603e6f21ad
John Koht <john@kohactive.com>
2012-03-15 19:41:31 -0500

    updating the BHSI form length errors?

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120315220934) do
+ActiveRecord::Schema.define(:version => 20120316003041) do
 
   create_table "affiliate_event_applications", :force => true do |t|
     t.string   "first_name",           :null => false
