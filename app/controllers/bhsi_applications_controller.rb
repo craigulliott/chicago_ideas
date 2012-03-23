826b742f4bcc89167f28a5f658e8cc7f57d8474c
John Koht <john@kohactive.com>
2012-03-22 23:07:57 -0500

    updatin gbhsi

--- app/controllers/bhsi_applications_controller.rb
+++ app/controllers/bhsi_applications_controller.rb
@@ -14,7 +14,8 @@ class BhsiApplicationsController < ApplicationController
     @meta_data = {:page_title => "Bluhm/Helfand Social Innovation Fellowship", :og_image => "http://www.chicagoideas.com/assets/application/affilliate_events_banner.jpg", :og_title => "Bluhm/Helfand Social Innovation Fellowship | Chicago Ideas Week", :og_type => "website", :og_desc => "Chicago Ideas Week (CIW) is about the sharing of ideas, inspiring action and igniting change to positively impact our world. People who come to CIW are artists, engineers, technologists, inventors, scientists, musicians, economists, explorers-and, well...just innately passionate."}
     #if current_user and current_user.bhsi_application.present?
     if current_user and current_user.bhsi_application.present?
-      redirect_to root_path, :notice => 'Thank you, your application has already been recieved.'
+      flash[:notice] = 'Thank you, your application has already been recieved.'
+      redirect_to root_path
     elsif current_user
       @bhsi_application = current_user.build_bhsi_application
     else
@@ -45,7 +46,8 @@ class BhsiApplicationsController < ApplicationController
       end
       
     else
-      redirect_to root_path, :notice => 'Thank you, your application has already been recieved.' 
+      flash[:notice] = 'Thank you, your application has already been recieved.'
+      redirect_to root_path
     end
     
   end
