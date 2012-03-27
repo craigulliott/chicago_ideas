d0252e8533e6ed211f79680036ed07f28393da52
John Koht <johnkoht@John-Kohts-MacBook-Pro.local>
2012-03-27 01:42:32 -0500

    updating the js for the blog

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,35 +17,39 @@ $(document).ready(function(){
   
   Modernizr.load();
   
-  $.get('/account_links', function(data) {
+  // Load the account links via AJAX unless it's the blog hosted on Tumblr
+  if ( $('body').not('.blog') ) {
     
-    if( data.admin ) {
-      $('#admin_link').show();
-    }
+    $.get('/account_links', function(data) {
     
-    if( data.newsletter ) {
-      $('#subscribe_to_newsletter').hide();
-      subscribe_to_newsletter
-    }
+      if( data.admin ) {
+        $('#admin_link').show();
+      }
     
-    if( data.signed_in ) {
-      $('#nav_user').show();
-      // dont ask for email or name when prompting for newsletter
-      // we use remove, so they are subsequently ignored when checking for required fields
-      $('#subscribe_to_newsletter input[type="text"]').remove();
-      $('#subscribe_to_newsletter input[type="email"]').remove();
+      if( data.newsletter ) {
+        $('#subscribe_to_newsletter').hide();
+        subscribe_to_newsletter
+      }
+    
+      if( data.signed_in ) {
+        $('#nav_user').show();
+        // dont ask for email or name when prompting for newsletter
+        // we use remove, so they are subsequently ignored when checking for required fields
+        $('#subscribe_to_newsletter input[type="text"]').remove();
+        $('#subscribe_to_newsletter input[type="email"]').remove();
       
       
-    } else {
-      $('#gn_register').show();
-      $('#gn_login').show();
-      $('#users_full_name').html(data.full_name);
-    }
+      } else {
+        $('#gn_register').show();
+        $('#gn_login').show();
+        $('#users_full_name').html(data.full_name);
+      }
         
-    if( data.connected_to_facebook ) {
-      $('.facebook_connect').hide();
-    }
+      if( data.connected_to_facebook ) {
+        $('.facebook_connect').hide();
+      }
         
-  }, 'JSON');
+    }, 'JSON');
+  }
   
 });
