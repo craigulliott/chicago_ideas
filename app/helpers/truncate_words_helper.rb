d1a2382a01604cda227e3537956d3565b10db6f3
John Koht <john@kohactive.com>
2012-04-03 18:33:23 -0500

    filterable events, talks and mega_talks by year

--- app/helpers/truncate_words_helper.rb
+++
@@ -1,8 +0,0 @@
-module TruncateWordsHelper
-
-  def truncate_words(text, length = 250, end_string = ' ...')
-    words = text.split()
-    words[0..(length-1)].join(' ') + (words.length > length ? end_string : '')
-  end
-  
-end
