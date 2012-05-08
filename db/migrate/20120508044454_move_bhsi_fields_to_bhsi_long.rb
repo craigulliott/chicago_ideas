42e9e4429db326f20ab815d5e5deb2960a50ac2b
Justin McNally <justin@justin-dev.local>
2012-05-08 00:16:21 -0500

    migrate to new object

--- db/migrate/20120508044454_move_bhsi_fields_to_bhsi_long.rb
+++ db/migrate/20120508044454_move_bhsi_fields_to_bhsi_long.rb
@@ -1,7 +1,27 @@
 class MoveBhsiFieldsToBhsiLong < ActiveRecord::Migration
   def up
-  end
-
-  def down
+    
+    BhsiApplication.all.each do |ba|
+      if ba.bhsi_longtext.nil?
+        
+        ba.build_bhsi_longtext()
+        ba.bhsi_longtext.about_yourself = ba.about_yourself
+        ba.bhsi_longtext.social_venture_description = ba.social_venture_description
+        ba.bhsi_longtext.venture_launched = ba.venture_launched
+        ba.bhsi_longtext.number_people_affected = ba.number_people_affected
+        ba.bhsi_longtext.explain_number = ba.explain_number
+        ba.bhsi_longtext.organizational_development = ba.organizational_development
+        ba.bhsi_longtext.makes_social_innovation = ba.makes_social_innovation
+        ba.bhsi_longtext.inspiration = ba.inspiration
+        ba.bhsi_longtext.improvements = ba.improvements
+        ba.bhsi_longtext.sustainability_model = ba.sustainability_model
+        ba.bhsi_longtext.distinguish_yourself = ba.distinguish_yourself
+        ba.bhsi_longtext.strong_midwest_connections_explained = ba.strong_midwest_connections_explained
+        ba.bhsi_longtext.additional_comments = ba.additional_comments
+        ba.bhsi_longtext.three_standout_statistics = ba.three_standout_statistics
+      
+      end
+    end
+  
   end
 end
