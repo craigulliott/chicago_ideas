f60b429281f168262d3e5921f6e19be254a9edd8
Dominic Bruno <dominicbruno@gmail.com>
2012-05-31 13:59:34 -0500

    Remove John Koht from mailers

--- app/mailers/community_partners_mailer.rb
+++ app/mailers/community_partners_mailer.rb
@@ -2,7 +2,6 @@ class CommunityPartnersMailer < ActionMailer::Base
 
   default :from => "forms@chicagoideas.com"
   default :to => "brooke@chicagoideas.com"
-  default :bcc => "john@kohactive.com"
   default :subject => "Community Partners Form Submission"
   
   def send_form(form, filename)
