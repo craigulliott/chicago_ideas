dcd8ab9d008f9b34fbafd226a33ac7511e4efd8c
Dominic Bruno <dominicbruno@gmail.com>
2012-07-12 10:45:28 -0500

    Start of CIW Calendar

--- config/routes.rb
+++ config/routes.rb
@@ -110,6 +110,9 @@ CraigsAdmin::Application.routes.draw do
     match 'speakers', :to => 'users#list_speakers'
     match 'speakers/edison', :to => 'users#list_edison_speakers'
     match 'speakers/project_youth', :to => 'users#list_project_youth_speakers'
+    match 'calendar', :to => 'calendar#index'
+    match ':month_id/:day_id/calendar', :to => 'calendar#index'
+    
     
     resources :talks, :only => [:index] do
       # home pages for the different talk types
