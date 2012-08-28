04b3bfcff998a7177fc5343ecd8668e73410b73a
Dominic Bruno <dominicbruno@gmail.com>
2012-08-28 16:00:22 -0500

    Final Members page changes

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -5,11 +5,12 @@ class MembersController < ApplicationController
   
   def index
     if params[:year_id].present?
-      @members = Member.search_sort_paginate(params)
-      
-      @patrons = Member.joins([:year, :member_type]).where("years.id = '#{params[:year_id]}' AND member_types.name = 'Patron'").sort_by(&:get_last_name)
+     
       @pillars = Member.joins([:year, :member_type]).where("years.id = '#{params[:year_id]}' AND member_types.name = 'Pillar'").sort_by(&:get_last_name)
       @benefactors = Member.joins([:year, :member_type]).where("years.id = '#{params[:year_id]}' AND member_types.name = 'Benefactor'").sort_by(&:get_last_name)
+      @patrons = Member.joins([:year, :member_type]).where("years.id = '#{params[:year_id]}' AND member_types.name = 'Patron'").sort_by(&:get_last_name)
+      
+      @members = @patrons + @benefactors + @patrons
       
     end
     @meta_data = {:page_title => "CIW Members", :og_title => "Chicago Ideas Week Members", :og_type => "website"}
